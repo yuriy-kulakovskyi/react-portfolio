005154de4e354dec8eaf00c11b65b2eefe22875e
yuriy-kulakovskyi <yurakulak42@gmail.com>
2023-04-18 21:39:37 +0300

    speed fix

--- src/components/About/Main/Info/Info.jsx
+++ src/components/About/Main/Info/Info.jsx
@@ -14,12 +14,12 @@ const Info = () => {
     <div className='info'>
       <div className="info__image-box">
         {/* img */}
-        <img src={process.env.PUBLIC_URL + info.img} alt="Avatar" />
+        <img src={info.img === undefined ? process.env.PUBLIC_URL + "/img/avatar.webp" : process.env.PUBLIC_URL + info.img} alt="Avatar" />
       </div>
 
       {/* Paragraph with className info__paragraph */}
       <p className="info__paragraph">
-        {info.paragraph}
+        {info.paragraph === undefined ? "I'm a creative, communicative and analytical front-end developer with strong resilience. I design aesthetic and user-friendly web interfaces, communicate effectively with team members and clients, analyze data to find optimal solutions, and deliver quality work even under high pressure and tight deadlines." : info.paragraph}
       </p>
     </div>
   );
